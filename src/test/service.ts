a93931b3c875f8f515967985baced679c0263e21
tavo <taannu@ttu.ee>
2020-03-13 13:11:24 +0200

    Small tweaks to nginx

--- src/test/service.ts
+++ src/test/service.ts
@@ -1,42 +1,37 @@
-import { autoinject } from "aurelia-framework";
+import {autoinject} from "aurelia-framework";
 import {HttpClient} from "aurelia-fetch-client";
 
 @autoinject
 export class Service {
-  constructor(private http: HttpClient) {}
-
-  getAll() {
-    let httpClient = new HttpClient();
-
-    httpClient.configure(config => {
-      config
-        .withBaseUrl('api/')
-        .withDefaults({
-          mode: 'no-cors',
-          credentials: 'same-origin',
-          headers: {
-            'Accept': 'application/json',
-            'X-Requested-With': 'Fetch'
-          }
-        })
-        .withInterceptor({
-          request(request) {
-            console.log(`Requesting ${request.method} ${request.url}`);
-            return request;
-          },
-          response(response) {
-            console.log(`Received ${response.status} ${response.url}`);
-            return response;
-          }
+    constructor(private http: HttpClient) {
+        this.http.configure(config => {
+            config
+                .withBaseUrl('api/')
+                .withDefaults({
+                    headers: {
+                        'Accept': 'application/json',
+                        'X-Requested-With': 'Fetch'
+                    }
+                })
+                .withInterceptor({
+                    request(request) {
+                        console.log(`Requesting ${request.method} ${request.url}`);
+                        return request;
+                    },
+                    response(response) {
+                        console.log(`Received ${response.status} ${response.url}`);
+                        return response;
+                    }
+                });
         });
-    });
+    }
 
-
-
-    httpClient.fetch("http://localhost:4567/api/invoices")
-      .then(response => {
-        console.log(response.json());
-
-      })
-  }
+    getAll() {
+        console.log("Getting data");
+        this.http.fetch("invoices")
+            .then(response => {
+                console.log(response);
+                console.log("json", response.json());
+            })
+    }
 }
