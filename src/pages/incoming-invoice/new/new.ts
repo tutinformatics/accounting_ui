2102d49a59896111b21855fdf2ed7058c76b2113
tavo <tavo.annus@gmail.com>
2020-04-05 22:17:07 +0300

    Some data binding

--- src/pages/incoming-invoice/new/new.ts
+++ src/pages/incoming-invoice/new/new.ts
@@ -1,3 +1,10 @@
-export class New {
+import {Invoice} from "../../../model/invoice";
 
+export class New {
+    invoice: Invoice = new Invoice();
+
+
+    createInvoice() {
+        console.log(this.invoice);
+    }
 }
