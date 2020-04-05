bb1e7601b7469908fb5d6d69a5aac9291260f502
tavo <tavo.annus@gmail.com>
2020-04-05 21:54:37 +0300

    Add missing fields

--- src/model/invoice.ts
+++ src/model/invoice.ts
@@ -2,14 +2,17 @@ import {Item} from "./item";
 
 export class Invoice {
     invoiceId: string;
+    partyIdFrom: string;
     partyId: string;
     dueDate: Date;
     invoiceTypeId: string;
     createdStamp: Date;
     description: string;
-    partyIdFrom: string;
     lastUpdatedStamp: Date;
     currencyUomId: string;
+    createdTxStamp: Date;
+    lastUpdatedTxStamp: Date;
+    invoiceDate: Date;
     statusId: string;
     items: [Item]; // TODO: some mapping, not sure of field name?
 }
