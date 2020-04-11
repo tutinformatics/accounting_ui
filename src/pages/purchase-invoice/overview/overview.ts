54591222a5ea5df97702a85cd7a4221b0bcb3a96
tavo <tavo.annus@gmail.com>
2020-04-11 17:18:07 +0300

    Format time

--- src/pages/purchase-invoice/overview/overview.ts
+++ src/pages/purchase-invoice/overview/overview.ts
@@ -1,10 +1,12 @@
 import {inject} from 'aurelia-framework';
 import {InvoiceService} from "../../../service/invoice-service";
+import {TimeConverter} from '../../../util/time-converter';
 
 @inject(InvoiceService)
 export class Overview {
 
     invoices = [];
+    sortAsc = true;
 
     constructor(private invoiceService: InvoiceService) {
         this.getInvoices();
@@ -12,8 +14,8 @@ export class Overview {
 
     public sortBy(prop) {
         this.invoices = this.invoices.sort((a, b) => {
-            if (a[prop] > b[prop]) return 1;
-            if (a[prop] < b[prop]) return -1;
+            if (a[prop] > b[prop]) return this.sortAsc ? 1 : -1;
+            if (a[prop] < b[prop]) return this.sortAsc ? -1 : 1;
             return 0;
         })
     }
@@ -23,10 +25,15 @@ export class Overview {
             .then(res => this.invoices = res);
     }
 
-    public getTotal(invoice) {
+    public convertTime(ms: number) {
+        return TimeConverter.convertDate(ms);
+    }
+
+    public getTotal(invoice): number {
         let amount = 0;
         for (let i = 0; i < invoice.invoiceItemIdValues.length; i++) {
             amount += invoice.invoiceItemIdValues[i].amount;
         }
+        return amount; // wtf is dis. no work?
     }
 }
