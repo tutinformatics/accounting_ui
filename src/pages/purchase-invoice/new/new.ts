798c0015162d03ca060cb5dfde7a3e7a8924a3f6
eghunt <eghunt@ttu.ee>
2020-04-12 03:21:14 +0300

    Name, submission date and days left to pay validation.

--- src/pages/purchase-invoice/new/new.ts
+++ src/pages/purchase-invoice/new/new.ts
@@ -1,17 +1,52 @@
 import {Invoice} from "../../../model/invoice";
 import {InvoiceService} from "../../../service/invoice-service";
 import {inject} from "aurelia-framework";
+import {ValidationRules, ValidationControllerFactory, ValidationController} from "aurelia-validation";
 
-@inject(InvoiceService)
+@inject(InvoiceService, ValidationControllerFactory, ValidationController)
 export class New {
     invoice: Invoice = new Invoice();
+    confirmString: String = "Kinnita";
+    confirmed: boolean = false;
+    createdTimeStamp = "";
+    valController: ValidationController;
+    daysTimeToPay = "";
 
-    constructor(private invoiceService: InvoiceService) {
+    constructor(private invoiceService: InvoiceService,private controller: ValidationControllerFactory) {
+        this.valController =  controller.createForCurrentScope();
+        this.initRules();
+    }
+
+    initRules() {
+        ValidationRules
+            .ensure("createdStamp")
+            .required()
+            .matches(/^(0?[1-9]|[12][0-9]|3[01])[\/\-\.](0?[1-9]|1[012])[\/\-\.]\d{4}$/)
+            .on(this.invoice)
+            .ensure("partyIdFrom")
+            .matches(/^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/)
+            .maxLength(50)
+            .required()
+            .on(this.invoice)
+            .ensure("daysTimeToPay")
+            .required()
+            .matches(/^[1-9]|[1-9][0-9]$/)
+            .on(this);
     }
 
     createInvoice() {
         // TODO: Validate @Marten
         this.invoiceService.createInvoice(this.invoice);
         // TODO: Handle error, etc @Tavo
+    }
+
+    confirm() {
+        if (this.confirmed) {
+            this.confirmed = !this.confirmed;
+            this.confirmString = "Kinnita";
+        } else {
+            this.confirmed = !this.confirmed;
+            this.confirmString = "Kinnitatud"
+        }
     }
 }
