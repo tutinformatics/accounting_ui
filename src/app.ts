bf5a378137767c8cd9d6777f07ef34ee05b2efe0
likutt <likutt@ttu.ee>
2020-04-26 23:37:52 +0300

    added mf-app

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ export class app {
     config.options.root = '/';
     config.map([
       {route:[ "login"], name: 'login', moduleId: PLATFORM.moduleName('pages/login/login'), nav: false, title:'Login'},
-      {route:[ "", "home"], name: 'home', moduleId: PLATFORM.moduleName('pages/home/mf-home'), nav: false, title:'Home'},
+      {route:[ "", "home"], name: 'home', moduleId: PLATFORM.moduleName('pages/home/home'), nav: false, title:'Home'},
       {route:["order/overview"], name: 'order/overview', moduleId: PLATFORM.moduleName('pages/order/overview/overview'), nav: true, title:'Order'},
       {route:["order/new"], name: 'order/new', moduleId: PLATFORM.moduleName('pages/order/new/new'), nav: true, title:'Order'},
       {route:["purchase-invoice/new"], name: 'purchase-invoice/new', moduleId: PLATFORM.moduleName('pages/purchase-invoice/new/new'), nav: true, title:'arve'},
@@ -29,10 +29,7 @@ export class app {
       {route:["sales-invoice/new"], name: 'sales-invoice/new', moduleId: PLATFORM.moduleName('pages/sales-invoice/new/new'), nav: true, title:'arve'},
       {route:["sales-invoice/overview"], name: 'sales-invoice/overview', moduleId: PLATFORM.moduleName('pages/sales-invoice/overview/overview'), nav: true, title:'arve'},
       {route:["customer/customers"], name: 'customer/customers', moduleId: PLATFORM.moduleName('pages/customer/customers/customers'), nav: true, title:'customers'},
-      {route:["customer/new"], name: 'customer/new', moduleId: PLATFORM.moduleName('pages/customer/new/new'), nav: true, title:'New Customer'},
-      {route:["order/mf-active-orders"], name: 'order/mf-active-orders', moduleId: PLATFORM.moduleName('pages/order/mf-active-orders/mf-active-orders'), nav: true, title:'Active orders'},
-      {route:["order/mf-orders-history"], name: 'order/mf-orders-history', moduleId: PLATFORM.moduleName('pages/order/mf-orders-history/mf-orders-history'), nav: true, title:'Orders history'},
-      {route:["order/mf-new-order"], name: 'order/mf-new-order', moduleId: PLATFORM.moduleName('pages/order/mf-new-order/mf-new-order'), nav: true, title:'New order'},
+      {route:["customer/new"], name: 'customer/new', moduleId: PLATFORM.moduleName('pages/customer/new/new'), nav: true, title:'New Customer'}
 
     ]);
 
