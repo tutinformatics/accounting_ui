e795d8d73a6a432c8aead731099f0e33683ce37c
likutt <likutt@ttu.ee>
2020-05-08 15:58:57 +0300

    product view, updating product view, a routing fix

--- src/pages/mf-products/mf-change-product/mf-change-product.ts
+++ src/pages/mf-products/mf-change-product/mf-change-product.ts
@@ -1,7 +1,39 @@
-export class MfChangeProduct {
-    message: string;
+import {inject} from "aurelia-dependency-injection";
+import {ValidationController, ValidationControllerFactory} from "aurelia-validation";
+import {Product} from "../../../model/product";
+import {ProductService} from "../../../service/product-service";
 
-    constructor() {
-        this.message = 'Hello world';
+@inject(ProductService, ValidationControllerFactory, ValidationController)
+export class New {
+
+    product = new Product();
+    controller = null;
+
+    constructor(private productService: ProductService, validationControllerFactory) {
+        this.controller = validationControllerFactory.createForCurrentScope();
+        this.initRules()
+    }
+
+    initRules() {
+    }
+
+    update() {
+        if (this.isValidated()) {
+            /*
+            this.productService.create(this.product)
+                .then(() => this.product = new Product())
+
+             */
+            window.location.href = "/mf-products/mf-product-view"
+        }
+
+    }
+
+
+    isValidated() {
+        if (this.controller.errors.length > 0) {
+            return false
+        }
+        return true
     }
 }
