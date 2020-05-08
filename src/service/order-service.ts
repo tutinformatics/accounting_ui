f6125cc1d690329f8629b8b01c35225ff5a41cc2
Kadri Männimets <kamann@ttu.ee>
2020-05-08 22:43:03 +0300

    Fixed order endpoint.

--- src/service/order-service.ts
+++ src/service/order-service.ts
@@ -4,10 +4,18 @@ import {Order} from "../model/order";
 export class OrderService extends Service {
 
     getAll() {
-        return this.get("/entities/Order");
+        return this.get("/entities/OrderContent");
     }
 
     create(order: Order) {
-        return this.post('/entities/Order', order.toJson())
+        return this.post('/entities/OrderContent', order.toJson())
+    }
+
+    update(order: Order) {
+        return this.put('/entities/OrderContent', order.toJson())
+    }
+
+    deleteOrder(order: Order) {
+        return this.delete('/entities/OrderContent', order.toJson())
     }
 }
